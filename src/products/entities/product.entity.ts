d4b4f76c78ab565316f16af6f27373e70d327c74
Cristian Kreuz Engel <kreuzengelc@gmail.com>
2024-10-13 21:40:44 -0300

    esboco tela produto

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,54 +1,60 @@
-import {Column, DataType, Model, Table} from "sequelize-typescript";
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
-@Table({tableName: 'products'})
-export class Product extends Model<Product>{
-    @Column({
-        type: DataType.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    })
-    id: number;
+@Table({ tableName: 'products' })
+export class Product extends Model<Product> {
+  @Column({
+    type: DataType.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
 
-    @Column({
-        type: DataType.STRING(255),
-        allowNull: false,
-    })
-    name: string;
+  @Column({
+    type: DataType.STRING(255),
+    allowNull: false,
+  })
+  name: string;
 
-    @Column({
-        type: DataType.DECIMAL(10,2),
-        allowNull: false,
-    })
-    price: number;
+  @Column({
+    type: DataType.DECIMAL(10, 2),
+    allowNull: false,
+  })
+  price: number;
 
-    @Column({
-        type: DataType.STRING(4),
-        allowNull: false,
-    })
-    voltage: string;
+  @Column({
+    type: DataType.STRING(4),
+    allowNull: false,
+  })
+  voltage: string;
 
-    @Column({
-        type: DataType.STRING(255),
-        allowNull: false,
-        unique: true,
-    })
-    cod: string;
+  @Column({
+    type: DataType.STRING(255),
+    allowNull: false,
+    unique: true,
+  })
+  cod: string;
 
-    @Column({
-        type: DataType.STRING(255),
-        allowNull: false,
-    })
-    image: string;
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+  })
+  isActive: string;
 
-    @Column({
-        type: DataType.DATE,
-        allowNull: false,
-    })
-    createdAt: string;
+  @Column({
+    type: DataType.STRING(255),
+    allowNull: false,
+  })
+  image: string;
 
-    @Column({
-        type: DataType.DATE,
-        allowNull: false,
-    })
-    updatedAt: string;
+  @Column({
+    type: DataType.DATE,
+    allowNull: false,
+  })
+  createdAt: string;
+
+  @Column({
+    type: DataType.DATE,
+    allowNull: false,
+  })
+  updatedAt: string;
 }
